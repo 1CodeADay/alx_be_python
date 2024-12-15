3512e55138da5c4b2da3abba7d2217a46a049ed5
Rodrigue Danso <rodrig.danso@gmail.com>
2024-12-15 20:36:45 +0100

    Create repo for Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum = number1 + number2
+
+#Difference
+difference = number1 - number2
+
+# Multiplication
+product = number1 * number2
+
+# Print the results
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+ 
