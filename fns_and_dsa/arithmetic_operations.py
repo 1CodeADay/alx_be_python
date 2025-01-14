146d8500304d0f0e099fb5f2d7983ff503ac5eb1
Rodrigue Danso <rodrig.danso@gmail.com>
2025-01-14 21:43:35 +0100

    First attempt to solve Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+
+def perform_operation(num1, num2, operation('add', 'subtract', 'multiply', 'divide')):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."
--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -14,4 +14,3 @@ product = number1 * number2
 print("Addition of", number1, "and", number2, "is", sum)
 print("Subtraction of", number1, "and", number2, "is", difference)
 print("Multiplication of", number1, "and", number2, "is", product)
- 
