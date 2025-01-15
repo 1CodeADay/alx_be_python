62f157c8e68ed22dc2c244c5b3d78fb69c73ade5
Rodrigue Danso <onecodeaday.ocad@gmail.com>
2025-01-15 04:45:42 +0100

    Complete Explore  Module task

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+# Part 1 - Display current date and time
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current date and time: ", current_date)
+
+
+# Part 2 - Calculate a Future Date
+days_to_add = int(input("Enter the number of days to add to the current date:"))
+def calculate_future_date(days_to_add):
+    future_date = datetime.now() + timedelta(days=days_to_add)
+    print(f"Future date after adding {days_to_add} days: ", future_date)
+calculate_future_date(days_to_add)
